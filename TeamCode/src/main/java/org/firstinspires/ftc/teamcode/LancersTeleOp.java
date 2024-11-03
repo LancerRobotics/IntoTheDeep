03e9e0a86192a22ef33f98e38b2817318a59b698
Parker Wahle <regulad@regulad.xyz>
2024-11-03 08:40:03 -0500

    restore import
    Signed-off-by: alm0st <alan.roy43@outlook.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LancersTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LancersTeleOp.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.acmerobotics.dashboard.config.Config;
-//TEMPORARY FOR COMP
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
