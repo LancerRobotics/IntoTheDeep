98aad562abdf50ab5f5f69f0c81f6f7305094e05
alm0st <alan.roy43@outlook.com>
2025-02-28 16:33:20 -0500

    Update auton testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,12 +15,26 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(25, -61.5, Math.toRadians(90)))
+                .strafeTo(new Vector2d(33, -61.5))
+                .strafeTo(new Vector2d(33,0))
+                .strafeTo(new Vector2d(41, 0))
+                .strafeTo(new Vector2d(41, -55))
 
-                        .build()
+                .strafeTo(new Vector2d(41,0))
+                .strafeTo(new Vector2d(53,0))
+                .strafeTo(new Vector2d(53,-55))
+
+                .strafeTo(new Vector2d(53,0))
+                .strafeTo(new Vector2d(61,0))
+                .strafeTo(new Vector2d(61,-55))
+                .build()
+
         );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        myBot.setDimensions(16.5,16.5);
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutons/pushRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutons/pushRight.java
@@ -12,24 +12,24 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 public class pushRight extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(25, -61.5, Math.toRadians(90)));
 
         waitForStart(); // This is required in all autons!!!
 
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0,0,0))
-                        .strafeTo(new Vector2d(0, -8))
-                        .strafeTo(new Vector2d(60,-8))
-                        .strafeTo(new Vector2d(60, -16))
-                        .strafeTo(new Vector2d(10,-16))
+                        .strafeTo(new Vector2d(33, -61.5))
+                        .strafeTo(new Vector2d(33,0))
+                        .strafeTo(new Vector2d(41, 0))
+                        .strafeTo(new Vector2d(41, -55))
 
-                        .strafeTo(new Vector2d(60,-16))
-                        .strafeTo(new Vector2d(60,-28))
-                        .strafeTo(new Vector2d(10,-28))
+                        .strafeTo(new Vector2d(41,0))
+                        .strafeTo(new Vector2d(53,0))
+                        .strafeTo(new Vector2d(53,-55))
 
-                        .strafeTo(new Vector2d(60,-28))
-                        .strafeTo(new Vector2d(60,-36))
-                        .strafeTo(new Vector2d(10,-36))
+                        .strafeTo(new Vector2d(53,0))
+                        .strafeTo(new Vector2d(61,0))
+                        .strafeTo(new Vector2d(61,-55))
                         .build()
         );
 
